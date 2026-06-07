ab3f44c83ce09f6160d4bae09dd32d094fae4b4e
Oleksii <plechinta.a@gmail.com>
2026-06-07 22:53:59 +0200

    fix: BuyTires image overflow and text overlap on mobile

--- src/components/BuyTires/BuyTires.jsx
+++ src/components/BuyTires/BuyTires.jsx
@@ -6,9 +6,11 @@ function BuyTires({title}) {
         
                 <div className="mb-4">
                     <div className="tires-box">
-                        <span className="gs-text__h5" style={{color: "#000"}}>Buy tires from us!</span>
-                        <br />
-                        <span className="gs-text" style={{color: "#000"}}>Lorem ipsum lorem ipsum</span>
+                        <div className="tires-box__text">
+                            <span className="gs-text__h5" style={{color: "#000"}}>Buy tires from us!</span>
+                            <br />
+                            <span className="gs-text" style={{color: "#000"}}>Lorem ipsum lorem ipsum</span>
+                        </div>
                         <img src={tire_photo} className="tire_box__img" alt="" />
                     </div>
                 </div>
