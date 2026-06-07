a537aab8e6125f1dc0cdde998c3626267fa0406c
Oleksii <plechinta.a@gmail.com>
2026-06-07 23:00:17 +0200

    revert: BuyTires back to original

--- src/components/BuyTires/BuyTires.jsx
+++ src/components/BuyTires/BuyTires.jsx
@@ -3,13 +3,12 @@ import tire_photo from "../../assets/Image/buy_tires.png"
 
 function BuyTires({title}) {
     return (
-        
+
                 <div className="mb-4">
                     <div className="tires-box">
-                        <div className="tires-box__text">
-                            <p className="tires-box__title">Buy tires from us!</p>
-                            <p className="tires-box__desc">Lorem ipsum lorem ipsum</p>
-                        </div>
+                        <span className="gs-text__h5" style={{color: "#000"}}>Buy tires from us!</span>
+                        <br />
+                        <span className="gs-text" style={{color: "#000"}}>Lorem ipsum lorem ipsum</span>
                         <img src={tire_photo} className="tire_box__img" alt="" />
                     </div>
                 </div>
